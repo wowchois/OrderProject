9b4e0fb1e486f826a8878ee88ebc9d74100b8d80
chois <61316086+wowchois@users.noreply.github.com>
2022-10-20 00:32:34 +0900

    feat : enum타입 주문 상태 관리 추가
    enum타입 주문 상태 관리 추가

---
+++ src/main/java/com/chois/payorder/order/entity/OrderStatusConverter.java
@@ -0,0 +1,22 @@
+package kr.co.programmers.order.orders.domain;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.EnumSet;
+import java.util.NoSuchElementException;
+
+@Converter
+public class OrderStatusConverter implements AttributeConverter<OrderStatus,String> {
+    @Override
+    public String convertToDatabaseColumn(OrderStatus attribute) {
+        return attribute.getStatCode();
+    }
+
+    @Override
+    public OrderStatus convertToEntityAttribute(String dbData) {
+        return EnumSet.allOf(OrderStatus.class).stream()
+                .filter(e -> e.getStatCode().equals(dbData))
+                .findAny()
+                .orElseThrow(()-> new NoSuchElementException());
+    }
+}
